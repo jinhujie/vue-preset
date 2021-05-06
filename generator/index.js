8952db1a50ee91fae5a250575c78ded9a8975ea5
jinhujie <541537767@qq.com>
2021-05-06 11:45:03 +0800

    add: unit-test

--- generator/index.js
+++ generator/index.js
@@ -2,12 +2,13 @@ module.exports = api => {
   api.render('./template');
   api.extendPackage({
     devDependencies: {
-      "axios": "^0.21.1",
-      "swrv": "^1.0.0-beta.8",
+      // "axios": "^0.21.1",
+      // "swrv": "^1.0.0-beta.8",
       "less": "^4.1.1",
       "less-loader": "7",
       "jsonp": "^0.2.1",
       "vue-cli-plugin-style-resources-loader": "^0.1.5",
+      "@types/jest": "^24.0.19",
       "style-resources-loader": "^1.4.1"
     }
   })
--- generator/template/src/view/Sample.vue
+++ generator/template/src/view/Sample.vue
@@ -5,6 +5,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { generateClassWithDevice } from "@/util/device";
+import Service from "@/service/service";
 
 export default defineComponent({
   setup() {
